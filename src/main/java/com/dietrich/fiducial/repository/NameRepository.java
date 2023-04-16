035b2b91d2caaaf3495a58392987b87db2c6ea39
rafael <rafael.ldietrich@gmail.com>
2023-04-16 18:42:25 -0300

    Add @Query to NameRepository.java

--- src/main/java/com/dietrich/fiducial/repository/NameRepository.java
+++ src/main/java/com/dietrich/fiducial/repository/NameRepository.java
@@ -2,11 +2,13 @@ package com.dietrich.fiducial.repository;
 
 import com.dietrich.fiducial.model.Name;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NameRepository extends JpaRepository<Name, Long> {
 
+    @Query("SELECT COUNT(n.id) > 0 FROM Name n WHERE n.name = ?1")
     boolean existsByName(String name);
 
 }
