e88a75a380d8d6c52c3c5bd2df7e0b5d1ddcc30c
rafael <rafael.ldietrich@gmail.com>
2023-04-16 12:14:28 -0300

    first commit

---
+++ src/main/java/com/dietrich/fiducial/FiducialApplication.java
@@ -0,0 +1,13 @@
+package com.dietrich.fiducial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiducialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FiducialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dietrich/fiducial/FiducialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dietrich.fiducial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiducialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
